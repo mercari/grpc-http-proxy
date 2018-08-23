43562ce226b310f636d5afdb94d166fbcaabb0f3
Tomoya Tabuchi <t@tomoyat1.com>
2018-08-23 12:05:45 +0900

    Use zap/zaptest/observer to test log output

--- http/middleware_test.go
+++ http/middleware_test.go
@@ -3,9 +3,12 @@ package http
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
-	"github.com/mercari/grpc-http-proxy/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
 )
 
 func TestServer_withAccessTokenInvalidToken(t *testing.T) {
@@ -15,6 +18,8 @@ func TestServer_withAccessTokenInvalidToken(t *testing.T) {
 		contentType   string
 		token         string
 		expectedToken string
+		message       string
+		fields        []zapcore.Field
 	}{
 		{
 			name:          "no token",
@@ -22,6 +27,14 @@ func TestServer_withAccessTokenInvalidToken(t *testing.T) {
 			contentType:   "",
 			token:         "",
 			expectedToken: "foo",
+			message:       "unauthorized",
+			fields: []zapcore.Field{
+				{
+					Key:    "reason",
+					Type:   zapcore.StringType,
+					String: "no token",
+				},
+			},
 		},
 		{
 			name:          "wrong token",
@@ -29,12 +42,21 @@ func TestServer_withAccessTokenInvalidToken(t *testing.T) {
 			contentType:   "",
 			token:         "bar",
 			expectedToken: "foo",
+			message:       "unauthorized",
+			fields: []zapcore.Field{
+				{
+					Key:    "reason",
+					Type:   zapcore.StringType,
+					String: "invalid token",
+				},
+			},
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(*testing.T) {
-			server := New(tc.expectedToken, log.NewDiscard())
+			logger, logs := observer.New(zapcore.InfoLevel)
+			server := New(tc.expectedToken, zap.New(logger))
 			rr := httptest.NewRecorder()
 			handlerF := server.withAccessToken(func(w http.ResponseWriter, r *http.Request) {
 				panic("this shouldn't be called")
@@ -55,13 +77,64 @@ func TestServer_withAccessTokenInvalidToken(t *testing.T) {
 			if got, want := contentType, tc.contentType; got != want {
 				t.Fatalf("got %s, want %s", got, want)
 			}
+
+			if logs.Len() != 1 {
+				t.Fatalf("incorrect number of log entries: got %d, want %d", logs.Len(), 1)
+			}
+			entry := logs.All()[0]
+			if got, want := entry.Message, tc.message; got != want {
+				t.Fatalf("got: %s, want: %s", got, want)
+			}
+			for i, f := range entry.Context {
+				if got, want := tc.fields[i], f; !reflect.DeepEqual(got, want) {
+					t.Fatalf("got %v, want %v", got, want)
+				}
+			}
 		})
 	}
 }
 
 func TestServer_withLog(t *testing.T) {
-	server := New("foo", log.NewDiscard())
+	fields := []zapcore.Field{
+		{
+			Key:    "host",
+			Type:   zapcore.StringType,
+			String: "",
+		},
+		{
+			Key:    "path",
+			Type:   zapcore.StringType,
+			String: "/",
+		},
+		{
+			Key:     "status",
+			Type:    zapcore.Int64Type,
+			Integer: http.StatusOK,
+		},
+		{
+			Key:    "method",
+			Type:   zapcore.StringType,
+			String: http.MethodGet,
+		},
+	}
+	logger, logs := observer.New(zapcore.InfoLevel)
+	server := New("foo", zap.New(logger))
 	rr := httptest.NewRecorder()
-	handlerF := server.withLog(func(w http.ResponseWriter, r *http.Request) {})
-	handlerF(rr, httptest.NewRequest("GET", "/", nil))
+	handlerF := server.withLog(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	handlerF(rr, httptest.NewRequest(http.MethodGet, "/", nil))
+
+	if logs.Len() != 1 {
+		t.Fatalf("incorrect number of log entries: got %d, want %d", logs.Len(), 1)
+	}
+	entry := logs.All()[0]
+	if got, want := entry.Message, "request"; got != want {
+		t.Fatalf("got: %s, want: %s", got, want)
+	}
+	for i, f := range entry.Context {
+		if got, want := fields[i], f; !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %v, want %v", got, want)
+		}
+	}
 }
