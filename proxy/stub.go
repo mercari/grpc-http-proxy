c8fa36fde4da44e6e9b58448ceedb830bb0c3dfa
Tomoya Tabuchi <t@tomoyat1.com>
2018-09-06 19:18:04 +0900

    Move ClientConn to it's own file

--- proxy/stub.go
+++ proxy/stub.go
@@ -3,7 +3,6 @@ package proxy
 import (
 	"context"
 	"fmt"
-	"net/url"
 
 	"github.com/jhump/protoreflect/dynamic/grpcdynamic"
 	"google.golang.org/grpc"
@@ -14,35 +13,6 @@ import (
 	"github.com/mercari/grpc-http-proxy"
 	"github.com/mercari/grpc-http-proxy/errors"
 )
-
-// clientConn is a connection to a backend, with the ability to perform reflection
-type clientConn struct {
-	cc *grpc.ClientConn
-}
-
-// newClientConn creates a new clientConn
-func newClientConn(
-	ctx context.Context,
-	target *url.URL) (*clientConn, error) {
-	cc, err := grpc.DialContext(ctx, target.String(), grpc.WithInsecure())
-	if err != nil {
-		return nil, &errors.Error{
-			Code:    errors.UpstreamConnFailure,
-			Message: fmt.Sprintf("failed to connect to backend %s", target),
-		}
-	}
-	return &clientConn{
-		cc: cc,
-	}, nil
-}
-
-func (c *clientConn) target() string {
-	return c.cc.Target()
-}
-
-func (c *clientConn) close() error {
-	return c.cc.Close()
-}
 
 type stub struct {
 	stub grpcdynamic.Stub
